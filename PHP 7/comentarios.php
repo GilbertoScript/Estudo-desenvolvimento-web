77db4f5217f9bce14cbbd877f67f8dd4338addfe
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-02 15:58:36 -0300

    Add files via upload, and update files

---
+++ PHP 7/comentarios.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 4 - PHP - Comentários</title>
+</head>
+<body>
+
+	<!-- Algo que se utiliza bastante em diversas linguagens, são os comentários, o que por acaso é o que este texto é agora, comentários em PHP podem ser feitos de 3 formas -->
+
+	<?php 
+
+		// Comentário de uma única linha, sua sintaxe é de duas barras em sequência
+
+		# Comentário de uma única linha do tipo 'shell', e sua sintaxe é basicamente o sinal de hashtag
+
+		/*
+
+			E se for preciso fazer um comentário de diversas linhas.
+			Basta utilizar o modelo de múltiplas linhas,  sua sintaxe é basicamente:
+			barra asterisco(para iniciar), asterisco barra(para fechar), assim como no css.
+
+		*/
+
+	?>
+
+</body>
+</html>
