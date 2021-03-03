7761806c40a3de43054b7d17dee1d413b55282bc
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-03 15:33:06 -0300

    Add file via upload

---
+++ PHP 7/if-else-parte-1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 10 - PHP - if/else</title>
+</head>
+<body>
+
+	<?php 
+
+		// If/else é um operador de condições
+		// If = se, else = senão
+
+		############ Primeiro exemplo de um if/else
+		###########################################
+
+		if(condicação) {
+			// Caso a condição for verdadeira
+			// Trecho de código a ser executado
+
+		} else {
+			// Caso a condição for falsa
+			// Trecho de código a ser executado
+		}
+
+		############ Segundo exemplo de um if/else
+		##########################################
+
+		if(condicação) {
+			// Trecho de código a ser executado
+
+		} else if(condicação) {
+			// Trecho de código a ser executado
+
+		} else {
+			// Trecho de código a ser executado
+		}
+
+	?>
+
+</body>
+</html>
