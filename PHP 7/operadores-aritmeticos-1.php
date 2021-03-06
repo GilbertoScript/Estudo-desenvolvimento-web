4a38026aff51337a674e03b3db9d913d1d31d2cd
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-06 20:02:21 -0300

    Add files via upload

---
+++ PHP 7/operadores-aritmeticos-1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 21 - PHP - Operadores aritméticos 1</title>
+
+	<style type="text/css">
+		
+	</style>
+
+</head>
+<body>
+
+  	<?php 
+
+            /*
+
+                  -- Operadores aritméticos
+
+                  -- O que são operadores aritméticos?
+
+                  -- Operadores:
+
+                        Adição ( + ) - Soma valores
+                        Subtração ( - ) - Diferença entre valores
+                        Multiplicação ( * ) - Produto dos valores
+                        Divisão ( / ) - Quociente dos valores
+                        Módulo ( % ) - Resto existente em uma operação de divisão
+
+            */
+
+  	?>
+
+</body>
+</html>
