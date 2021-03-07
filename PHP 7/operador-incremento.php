4ec816271478290e9f18a1ec6e25c9260690c219
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-07 14:29:05 -0300

    Add file via upload

---
+++ PHP 7/operador-incremento.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 24 - PHP - Operadores de incremento</title>
+
+	<style type="text/css">
+		
+	</style>
+
+</head>
+<body>
+
+    <h2>Operador de incremento</h2>
+    <hr><br>
+
+  	<?php 
+
+      /*
+  
+          -- Operador de incremento/decremento
+
+          -- O que são os operadores de incremento e decremento?
+
+          - Operadores:
+
+          Pré-incremento(++$a) - adiciona uma unidade antes de retornar $a
+          Pós-incremento($a++) - retorna a, e depois adiciona uma unidade
+
+          Pré-decremento(--$a) - diminui uma unidade antes de retornar a
+          Pós-decremento($a--) - retorna a, e depois diminui uma unidade
+  
+      */
+
+  	?>
+
+</body>
+</html>
