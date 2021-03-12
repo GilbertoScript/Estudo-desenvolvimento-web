a517a30ce783d84563d6bd51e830afe92887e291
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-12 14:12:31 -0300

    Add file via upload

---
+++ PHP 7/array-introducao.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 32 - PHP - Array introdução</title>
+
+	<style type="text/css">
+		
+	</style>
+
+</head>
+<body>
+
+  <h2>Array introdução</h2>
+
+    <?php
+
+      // Arrays são listas ordenadas, podem ser de variáveis, números, strings...
+      // Um exemplo da utilização do array é a seguinte:
+
+      # Lista de frutas com variáveis:
+
+      $fruta_1 = 'Banana';
+      $fruta_2 = 'Maça';
+      $fruta_3 = 'Morango';
+      $fruta_4 = 'Uva';
+
+      # Com um único array
+
+      $lista_frutas_1[1] = 'Banana';
+      $lista_frutas_1[2] = 'Maça';
+      $lista_frutas_1[3] = 'Morango';
+      $lista_frutas_1[4] = 'Uva';
+
+    ?>
+
+    
+
+</body>
+</html>
