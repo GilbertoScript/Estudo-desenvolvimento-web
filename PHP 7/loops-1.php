a7a033423e8adccf5928cfaa496633c9b5125768
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-17 13:43:40 -0300

    Add file via upload

---
+++ PHP 7/loops-1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Aula 38 - PHP - Loops parte 1</title>
+
+	<style type="text/css">
+		
+		html {
+			background: #3D3D3D;
+			color: whitesmoke;
+		}
+
+		hr {
+			border-color: whitesmoke;
+		}
+
+	</style>
+
+</head>
+<body>
+
+  	<h2>Loops parte 1</h2><hr>
+
+	<?php 
+
+		// Loops parte 1 - Introdução - O que são loops?
+
+		// Loops são laços de repetição que executam determinada tarefa enquanto determinada condição estiver sendo válida, por exemplo:
+
+		// Enquanto a !== b, faça isso :
+
+		// E alguns blocos esperam determinada parada ou limite, para que o laço pare de executar quando chegar em algum limite.
+
+	?>
+
+</body>
+</html>
