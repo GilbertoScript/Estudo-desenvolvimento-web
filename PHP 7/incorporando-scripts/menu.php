0cc138c39be1dd190133fdf06c12d87b592df37e
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-03-25 14:32:29 -0300

    Add files via upload

---
+++ PHP 7/incorporando-scripts/menu.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<style type="text/css">
+
+		ul {
+			display: flex;
+			justify-content: space-around;
+			list-style: none;
+		}
+
+		ul a {
+			text-decoration: none;
+		}
+
+		ul a:hover {
+			text-decoration: underline;
+		}
+
+	</style>
+
+	<title>Menu</title>
+</head>
+<body>
+	
+	<!-- Para o exemplo de incorporação de páginas em php, teremos um simples menu fictício: -->
+	<ul>
+		<li>
+			<a href="inicio.php">Início</a>
+		</li>
+		<li>
+			<a href="minha-rede.php">Minha rede</a>
+		</li>
+		<li>
+			<a href="vagas.php">Vagas</a>
+		</li>
+	</ul>
+	<hr />				
+
+</body>
+</html>
