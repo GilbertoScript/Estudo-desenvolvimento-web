c235e696276a3f7ae8a321f529087196d5e73d06
Gilberto Alves <alvesgilberto84@gmail.com>
2021-04-29 15:50:57 -0300

    Add files via upload

--- MySQL/filtrando-dados.sql
+++ MySQL/filtrando-dados.sql
@@ -7,3 +7,20 @@
 
 -- AND: Todas as operações de comparação devem ser verdadeiras.
 -- OR: Pelo menos uma das operações de comparação deve ser verdadeira.
+
+-- Pesquisa simples:
+-- Seleciona todos as linhas de dentro da tabela tb_alunos, onde o estado é igual à DF;
+SELECT 
+	* 
+FROM 
+	`tb_alunos` 
+WHERE
+	estado = 'DF';
+
+-- Outra pesquisa:
+SELECT 
+	* 
+FROM 
+	`tb_alunos` 
+WHERE 
+	idade > 18 AND idade < 60 AND interesse = 'Informática';
