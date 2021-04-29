cb837dc1d6f704d2e048582e8a4903b54b4a89c1
Gilberto Alves <alvesgilberto84@gmail.com>
2021-04-29 14:42:47 -0300

    Add file via upload

---
+++ MySQL/filtrando-dados.sql
@@ -0,0 +1,9 @@
+-- Filtrando registros WHERE --
+
+-- Para que serve um filtro? Basicamente é isso mesmo... Para filtrar registros, e ter informações mais precisas e necessárias.
+
+-- Operadores de comparação: =, <, >, <=, >=,
+-- Operadores lógicos:
+
+-- AND: Todas as operações de comparação devem ser verdadeiras.
+-- OR: Pelo menos uma das operações de comparação deve ser verdadeira.
