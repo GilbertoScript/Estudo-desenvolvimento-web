fe912438fa0dc3e1cbd5743b7f618a42d842a322
Gilberto Alves <alvesgilberto84@gmail.com>
2021-05-09 11:21:25 -0300

    Update file

--- MySQL/Loja virtual/aula-4-relacionamento-muitos-para-muitos.sql
+++ MySQL/Loja virtual/aula-4-relacionamento-muitos-para-muitos.sql
@@ -25,3 +25,9 @@ CREATE TABLE tb_pedidos(
 
 -- Agora iremos criar a tabela que irá fazer o relacionamento de muitos para muitos, essa tabela irá relacionar o contexto tb_pedidos e o contexto de tb_produtos...
 
+CREATE TABLE tb_pedidos_produtos(
+	id_pedido INT NOT NULL,
+    id_produto INT NOT NULL,
+    FOREIGN KEY(id_pedido) REFERENCES tb_pedidos(id_pedido),
+    FOREIGN KEY(id_produto) REFERENCES tb_produtos(id_produto)
+);
