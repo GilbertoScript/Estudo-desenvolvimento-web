07025c18c716c64971531e9254297c2aca421de9
Gilberto Alves <alvesgilberto84@gmail.com>
2021-05-20 15:09:55 -0300

    Updating file

--- PHP com PDO/index.php
+++ PHP com PDO/index.php
@@ -1,10 +1,36 @@
 <?php
 	
-	$dsn = 'mysql:host=localhost;db_name=php_com_pdo';
+	// Parâmetros a serem passados a classe PDO('<DATA SOURCE NAME - DSN>', '<user>', '<senha>')
+	$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
 	$usuario = 'root';
 	$senha = '';
 
-	// Parâmetros a serem passados a classe PDO('<DATA SOURCE NAME - DSN>', '<user>', '<senha>')
-	$conexao = new PDO($dsn, $usuario, $senha)
+	try {
 
+		$conexao = new PDO($dsn, $usuario, $senha);	
+
+		$query = '
+			create table if not exists tb_usuarios(
+				id int not null primary key auto_increment,
+				nome varchar(50) not null,
+				email varchar(100) not null,
+				senha varchar(16) not null
+			)
+		';
+
+		// Retorno esperado é 0, isso pois o método exec retorna a quantidade de linhas que foram alteradas durante a execução da query, como executamos uma query pertencente ao subgrupo DDL(Data Definition Language), o retorno é 0 pois não foi afetado nenhuma linha, e sim criadas.
+		$retorno = $conexao->exec($query);
+
+		$query = '
+			delete from tb_usuarios
+		';
+
+		$retorno = $conexao->exec($query);
+		echo $retorno;
+
+	} catch(PDOException $e) {
+
+		echo "Erro: " . $e->getCode() . "<br />Mensagem: " . $e->getMessage();
+	}
+		
 ?>
