3dc80d8a7dcf0990f37706aff68b4572a2023572
GilbertoASJ <alvesgilberto84@gmail.com>
2022-01-27 17:41:19 -0300

    Feature: :sparkles: 534 class added - routes with slim

--- API com Slim/index.php
+++ API com Slim/index.php
@@ -1,20 +1,79 @@
 <?php
- 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
- 
-require'vendor/autoload.php';
- 
+
+require 'vendor/autoload.php';
+
 $app = new \Slim\App;
- 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
- 
-    $name = $args['name'];
- 
-    $response->getBody()->write("Hello, $name");
- 
-    return $response;
- 
+
+$app->get('/postagens2', function(){
+
+	echo "Listagem de postagens";
+
+} );
+
+$app->get('/usuarios[/{id}]', function($request, $response){
+	$id = $request->getAttribute('id');
+
+	//Verificar se ID é valido e existe no BD
+
+	echo "Listagem de usuarios ou ID: " . $id;
+
+} );
+
+$app->get('/postagens[/{ano}[/{mes}]]', function($request, $response){
+	
+	$ano = $request->getAttribute('ano');
+	$mes = $request->getAttribute('mes');
+
+	//Verificar se ID é valido e existe no BD
+
+	echo "Listagem de postagens Ano: " . $ano . " mes: " . $mes;
+
+} );
+
+$app->get('/lista/{itens:.*}', function($request, $response){
+	
+	$itens = $request->getAttribute('itens');
+
+	//Verificar se ID é valido e existe no BD
+	//echo $itens;
+	var_dump(explode("/", $itens));
+
+} );
+
+
+/* Nomear rotas */
+$app->get('/blog/postagens/{id}', function($request, $response){
+	echo "Listar postagem para um ID ";
+})->setName("blog");
+
+$app->get('/meusite', function($request, $response){
+	
+	$retorno = $this->get("router")->pathFor("blog", ["id" => "10" ] );
+
+	echo $retorno;
+
 });
- 
+
+
+/* Agrupar rotas */
+$app->group('/v5', function(){
+	
+	$this->get('/usuarios', function(){
+		echo "Listagem de usuarios";
+	} );
+
+	$this->get('/postagens', function(){
+		echo "Listagem de postagens";
+	} );
+
+} );
+
+
+
+
+
+
+
+
+
 $app->run();
