7ba7b3f084f502daa67316e7ba0e40ae03fe645e
GilbertoASJ <alvesgilberto84@gmail.com>
2022-01-28 11:59:57 -0300

    Feature: :sparkles: 535 class added - types of requisitions

--- API com Slim/index.php
+++ API com Slim/index.php
@@ -1,9 +1,84 @@
 <?php
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
+
+/* Padrão PSR7 */
+$app->get('/postagens', function(Request $request, Response $response){
+
+	/* Escreve no corpo da resposta utilizando o padrão PSR7 */
+	$response->getBody()->write("Listagem de postagens");
+
+	return $response;
+
+} );
+
+/*
+Tipos de requisição ou Verbos HTTP
+
+get -> Recuperar recursos do servidor (Select)
+post -> Criar dado no servidor (Insert)
+put -> Atualizar dados no servidor (Update)
+delete -> Deletar dados do servidor (Delete)
+
+*/
+
+$app->delete('/usuarios/remove/{id}', function(Request $request, Response $response){
+
+	$id = $request->getAttribute('id');
+	
+	/*
+	Deletar do banco de dados com DELETE..
+	....
+	*/
+
+	return $response->getBody()->write( "Sucesso ao deletar: " . $id );
+
+} );
+
+$app->put('/usuarios/atualiza', function(Request $request, Response $response){
+
+	//Recupera post ($_POST)
+	$post  = $request->getParsedBody();
+	$id    = $post['id'];
+	$nome  = $post['nome'];
+	$email = $post['email'];
+
+	/*
+	Atualizar no banco de dados com UPDATE..
+	....
+	*/
+
+	return $response->getBody()->write( "Sucesso ao atualizar: " . $id );
+
+} );
+
+$app->post('/usuarios/adiciona', function(Request $request, Response $response){
+
+	//Recupera post ($_POST)
+	$post  = $request->getParsedBody();
+	$nome  = $post['nome'];
+	$email = $post['email'];
+
+	/*
+	Salvar no banco de dados com INSERT INTO..
+	....
+	*/
+
+	return $response->getBody()->write( "Sucesso" );
+
+} );
+
+
+$app->run();
+
+/*
 $app->get('/postagens2', function(){
 
 	echo "Listagem de postagens";
@@ -39,9 +114,9 @@ $app->get('/lista/{itens:.*}', function($request, $response){
 	var_dump(explode("/", $itens));
 
 } );
+*/
 
-
-/* Nomear rotas */
+/* Nomear rotas 
 $app->get('/blog/postagens/{id}', function($request, $response){
 	echo "Listar postagem para um ID ";
 })->setName("blog");
@@ -53,9 +128,9 @@ $app->get('/meusite', function($request, $response){
 	echo $retorno;
 
 });
+*/
 
-
-/* Agrupar rotas */
+/* Agrupar rotas 
 $app->group('/v5', function(){
 	
 	$this->get('/usuarios', function(){
@@ -68,12 +143,5 @@ $app->group('/v5', function(){
 
 } );
 
+*/
 
-
-
-
-
-
-
-
-$app->run();
