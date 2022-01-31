89e0944af92e4075e94c7cf48041f06077e2c033
GilbertoASJ <alvesgilberto84@gmail.com>
2022-01-31 17:42:45 -0300

    Feature: :sparkles: 537 class added - middleware, response and database

--- API com Slim/index.php
+++ API com Slim/index.php
@@ -2,6 +2,7 @@
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 
 require 'vendor/autoload.php';
@@ -12,12 +13,160 @@ $app = new \Slim\App([
 	] 
 ]);
 
-/* Container dependency injection */
+$container = $app->getContainer();
+$container['db'] = function(){
+
+	$capsule = new Capsule;
+	$capsule->addConnection([
+	    'driver'    => 'mysql',
+	    'host'      => 'localhost',
+	    'database'  => 'slim',
+	    'username'  => 'root',
+	    'password'  => '',
+	    'charset'   => 'utf8',
+	    'collation' => 'utf8_unicode_ci',
+	    'prefix'    => '',
+	]);
+
+	$capsule->setAsGlobal();
+	$capsule->bootEloquent();
+
+	return $capsule;
+
+};
+
+$app->get('/usuarios', function(Request $request, Response $response) {
+
+	$db = $this->get('db');
+	/*$db->schema()->dropIfExists('usuarios');
+	$db->schema()->create('usuarios', function($table){
+
+		$table->increments('id');
+		$table->string('nome');
+		$table->string('email');
+		$table->timestamps();
+
+	});*/
+
+	/* Inserir */
+	/*$db->table('usuarios')->insert([
+		'nome' => 'Jamilton Damasceno',
+		'email' => 'jamilton@gmail.com'
+	]);*/
+
+	/* Atualizar */
+	/*$db->table('usuarios')
+				->where('id', 1)
+				->update([
+					'nome' => 'Jamilton'
+				]);*/
+
+	/* Deletar */
+	/*$db->table('usuarios')
+				->where('id', 1)
+				->delete();*/
+
+	/* Listar */ 
+	$usuarios = $db->table('usuarios')->get();
+	foreach ($usuarios as $usuario) {
+		echo $usuario->nome . '<br>';
+	}
+
+	
+} );
+
+
+$app->run();
+
+
+/* Tipos de respostas 
+cabeçalho, texto, Json, XML
+
+
+$app->get('/header', function(Request $request, Response $response) {
+
+	$response->write('Esse é um retorno header');
+	return $response->withHeader('allow', 'PUT')
+			 ->withAddedHeader('Content-Length', 30);
+
+	
+} );
+
+$app->get('/json', function(Request $request, Response $response) {
+
+	return $response->withJson([
+		"nome" => "Jamilton Damasceno",
+		"endereco" => "Endereco tal...."
+	]);
+	
+} );
+
+
+$app->get('/xml', function(Request $request, Response $response) {
+
+	$xml = file_get_contents('arquivo');
+	$response->write($xml);
+
+	return $response->withHeader('Content-Type', 'application/xml' );
+	
+} );
+*/
+
+
+
+/* middleware 
+
+$app->add( function($request, $response, $next){
+
+	$response->write(' Inicio camada 1 + ');
+	//return $next($request, $response);
+	$response = $next($request, $response);
+
+	$response->write(' + Fim camada 1 ');
+	return $response;
+
+});
+
+$app->add( function($request, $response, $next){
+
+	$response->write(' Inicio camada 2 + ');
+	//return $next($request, $response);
+	$response = $next($request, $response);
+
+	$response->write(' + Fim camada 2 ');
+	return $response;
+
+});
+
+/*
+$app->add( function($request, $response, $next){
+
+	$response->write(' Inicio camada 2 + ');
+	return $next($request, $response);
+
+});*/
+
+/*
+$app->get('/usuarios', function(Request $request, Response $response) {
+
+	$response->write(' Ação principal usuarios ');
+	
+} );
+
+$app->get('/postagens', function(Request $request, Response $response) {
+
+	$response->write(' Ação principal postagens ');
+	
+} );
+*/
+
+
+/* Container dependency injection 
 class Servico {
 
 }
 
-/* Container Pimple */
+/* Container Pimple 
 $container = $app->getContainer();
 $container['servico'] = function(){
 	return new Servico;
@@ -30,15 +179,14 @@ $app->get('/servico', function(Request $request, Response $response) {
 	
 } );
 
-/* Controllers como serviço */
+/* Controllers como serviço 
 $container = $app->getContainer();
 $container['Home'] = function(){
 	return new MyApp\controllers\Home( new MyApp\View );
 };
 $app->get('/usuario', 'Home:index' );
+*/
 
-
-$app->run();
 
 
 /* Padrão PSR7 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <h3 align="center">Material para estudo Front-end</h3>
 
 <p align="center">
-  Meu material para estudo front-end contendo: <br />
-  HTML, CSS, Bootstrap, Javascript, ES6, ES7, ES8, ES9, ES10, ES11, ES12<br> PHP, Ajax, MySQL, jQuery, MVC, Sass e API com Slim Framework <br />
-  vou sempre atualizando de acordo   com o que vou estudando.
+    Material de estudo para Desenvolvimento Web contendo: <br />
+    HTML, CSS, Bootstrap, Javascript, ES6, ES7, ES8, ES9, ES10, ES11, ES12,<br> PHP, Ajax, MySQL, jQuery, MVC, Sass e API com Slim Framework <br />
+    vou sempre atualizando de acordo   com o que vou estudando.
 </p>
